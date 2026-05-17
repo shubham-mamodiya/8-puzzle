ed408b5c8f64930f08e915c46a13e8fed5f7c289
shubham-mamodiya <shubham.mamodiya.dev@gmail.com>
2026-05-17 13:05:48 +0530

    completed board api

--- src/board.rs
+++ src/board.rs
@@ -1,3 +1,6 @@
+use rand::seq::SliceRandom;
+use std::ptr;
+
 pub enum BoardError {
     InvalidDimension,
 }
@@ -38,5 +41,104 @@ impl Board {
 
     pub fn board(&self) -> &[u32] {
         &self.board
+    }
+
+    pub fn cal_index(&self, &row: &usize, &col: &usize) -> usize {
+        row * self.dimension + col
+    }
+
+    pub fn print_it(&self) {
+        for i in 0..self.dimension {
+            for j in 0..self.dimension {
+                let idx = self.cal_index(&i, &j);
+                let value = self.board[idx];
+                let c = if value == 0 {
+                    '_'.to_string()
+                } else {
+                    value.to_string()
+                };
+                print!(" {} ", c);
+            }
+            println!();
+        }
+    }
+
+    pub fn is_goal(&self) -> bool {
+        if self.board[self.size() - 1] != 0 {
+            return false;
+        }
+        for i in 1..self.size() {
+            if self.board[i - 1] != i as u32 {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    pub fn humming(&self) -> u32 {
+        let mut humming_distance: u32 = 0;
+        if self.board[self.size() - 1] != 0 {
+            humming_distance += 1;
+        }
+
+        for i in 1..self.size() {
+            if self.board[i - 1] != i as u32 {
+                humming_distance += 1;
+            }
+        }
+
+        humming_distance
+    }
+
+    pub fn manhattan(&self) -> u64 {
+        let mut manhattan: u64 = 0;
+
+        let dim = self.dimension as i64;
+        let mut expected_row: i64;
+        let mut expected_col: i64;
+        let mut current_row: i64;
+        let mut current_col: i64;
+
+        for i in 0..self.size() {
+            let tile = self.board[i] as i64;
+            if tile == 0 {
+                expected_row = dim - 1;
+                expected_col = dim - 1;
+            } else {
+                let tile = tile - 1;
+                expected_row = tile / dim;
+                expected_col = tile % dim;
+            }
+
+            current_row = i as i64 / dim;
+            current_col = i as i64 % dim;
+
+            manhattan += (expected_row - current_row).abs().unsigned_abs()
+                + (expected_col - current_col).unsigned_abs();
+        }
+        manhattan
+    }
+
+    pub fn shuffle(&mut self) {
+        let mut rng = rand::rng();
+        self.board.shuffle(&mut rng);
+    }
+
+    pub fn is_equal(&self, other: &Board) -> bool {
+        if ptr::eq(self, other) {
+            return true;
+        }
+
+        if self.size() != other.size() {
+            return false;
+        }
+
+        for i in 0..self.size() {
+            if self.board[i] != other.board[i] {
+                return false;
+            }
+        }
+        true
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use crate::board::Board;
 
 mod board;
 fn main() {
-    let b = match Board::new(0) {
+    let mut b = match Board::new(3) {
         Ok(board) => board,
         Err(_e) => {
             println!("Error creating board with size 1");
@@ -15,10 +15,26 @@ fn main() {
         }
     };
 
-    println!(
-        "board: {:?}\n size: {}\n dimension:{}",
-        b.board(),
-        b.size(),
-        b.dimension()
-    );
+    b.print_it();
+    println!("{}", b.humming());
+    let mut c = match Board::new(3) {
+        Ok(board) => board,
+        Err(_e) => {
+            println!("Error creating board with size 1");
+            match Board::new(1) {
+                Ok(board) => board,
+                Err(_e) => {
+                    return;
+                }
+            }
+        }
+    };
+    println!("{}", b.is_equal(&b));
+    println!("{}", b.is_equal(&c));
+
+    println!("{}", b.manhattan());
+    b.shuffle();
+
+    b.print_it();
+    println!("{}", b.manhattan());
 }
